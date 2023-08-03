c42c8ecd55e1ff7c0c5c65f4863766b8ae08bafc
91创业网 <132548947+gsj2023@users.noreply.github.com>
2023-08-03 17:56:18 +0800

    Update settings.tsx

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -517,7 +517,7 @@ export function Settings() {
             </span>
             {/* </Link> */}
           </ListItem>
-          <ListItem title="gpt 4.0当前剩余对话次数(每三小时刷新)">
+          <ListItem title="Ai 4.0当前剩余对话次数(每三小时刷新)">
             <span>{currentTime}</span>
           </ListItem>
 
