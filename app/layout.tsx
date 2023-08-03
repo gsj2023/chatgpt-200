5354bb269f91f10a2af636c939534f48cd360834
91创业网 <132548947+gsj2023@users.noreply.github.com>
2023-08-03 14:47:03 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT",
+    title: "Ai Robot",
     statusBarStyle: "default",
   },
 };
